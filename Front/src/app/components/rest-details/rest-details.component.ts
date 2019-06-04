6344a6085d6a01709193e80ca88bd1ab8a8af69e
jo3l17 <joelvaldezangeles@gmail.com>
2019-06-04 17:30:49 -0500

    Avance, vista rest-details 2.0
    comentarios=objeto
    abiert=boolean
    horarios terminados

--- Front/src/app/components/rest-details/rest-details.component.ts
+++ Front/src/app/components/rest-details/rest-details.component.ts
@@ -10,11 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RestDetailsComponent implements OnInit {
 
-  
-  title: string = 'La Sanguchera';
+  feriadors:Boolean=true;
+  estado: Boolean = true;
+  horarioFin: string = "22:00"
+  horarioInicio:string="12:00"
+  title: string = 'Anticucheria la ultima cena';
   lat: number = -16.4310132;
   lng: number = -71.5189799;
-
+  horarios:Array<String>=["lunes","martes","miercoles","Jueves","Viernes","Sabado","Domingo"]  
+  objComentario:any={
+    id:0,
+    nombre:"Joel",
+    comentario:"Muy buen restaurante",
+    rating:3
+  }
+  comentarios:Array<any>=[{
+    id:1,
+    nombre:"Joel",
+    comentario:"Recomendado",
+    rating:5
+  },
+  {
+    id:2,
+    nombre:"Juan Perez",
+    comentario:"Hace unos buenos anticuchos",
+    rating:4.5
+  },
+  {
+    id:3,
+    nombre:"Maria la del barrio",
+    comentario:"No me fue muy bien",
+    rating:1.5
+  }]
   constructor() {}
 
   ngOnInit() {
