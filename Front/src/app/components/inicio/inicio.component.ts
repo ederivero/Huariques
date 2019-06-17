0309f078c7e59b76cf99a8cd7301b275c0f4941c
jo3l17 <joelvaldezangeles@gmail.com>
2019-06-16 23:48:33 -0500

    inicio actualizado

--- Front/src/app/components/inicio/inicio.component.ts
+++ Front/src/app/components/inicio/inicio.component.ts
@@ -9,13 +9,20 @@ export class InicioComponent implements OnInit {
   title: string = 'Descubre nuevos lugares para comer!';
   lat: number = -16.4310132;
   lng: number = -71.5189799;
-  constructor() { }
+  num = 0;
+  constructor() {
+    this.contador()
+  }
 
   status: boolean = false;
-  abrirBusqueda(){
-      this.status = !this.status;       
+  abrirBusqueda() {
+    this.status = !this.status;
   }
-  
+  contador() {
+    for (let i = 0; i < 100; i++) {
+      setTimeout(()=>{this.num++},1000)
+    }
+  }
   ngOnInit() {
   }
 
